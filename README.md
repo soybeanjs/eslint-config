f9b2be21e45fc4d1d7be55097d0cc3af211a0dd9
Soybean <honghuangdc@gmail.com>
2022-08-26 02:24:07 +0800

    init(projects): init projects

---
+++ README.md
@@ -0,0 +1,3 @@
+## Soybean's eslint config presets      
+
+this eslint config contains basic(js、json、jsonc、md、yaml、yml), TS, Vue, React
---
+++ packages/base/index.js
@@ -0,0 +1,122 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2022,
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es2022: true
+  },
+  extends: ['./rules/all.js', 'plugin:jsonc/recommended-with-jsonc', 'plugin:prettier/recommended'],
+  plugins: ['html', '@html-eslint'],
+  ignorePatterns: [
+    'node_modules',
+    '*.min.*',
+    'CHANGELOG.md',
+    'dist',
+    'LICENSE*',
+    'output',
+    'coverage',
+    'public',
+    'temp',
+    'package-lock.json',
+    'pnpm-lock.yaml',
+    'yarn.lock',
+    '__snapshots__',
+    '!.github',
+    '!.vitepress',
+    '!.vscode'
+  ],
+  overrides: [
+    {
+      files: ['*.html'],
+      parser: '@html-eslint/parser',
+      extends: ['plugin:@html-eslint/recommended'],
+      rules: {
+        // https://yeonjuan.github.io/html-eslint/docs/all-rules
+        '@html-eslint/indent': 'off', // 由prettier控制
+        '@html-eslint/require-closing-tags': 'off',
+        '@html-eslint/no-extra-spacing-attrs': 'off',
+
+        // prettier
+        'prettier/prettier': ['error', { parser: 'html' }]
+      }
+    },
+    {
+      files: ['*.json', '*.json5', '*.jsonc'],
+      parser: 'jsonc-eslint-parser'
+    },
+    {
+      files: ['package.json'],
+      parser: 'jsonc-eslint-parser',
+      rules: {
+        'jsonc/sort-keys': [
+          'error',
+          {
+            pathPattern: '^$',
+            order: [
+              'publisher',
+              'name',
+              'displayName',
+              'type',
+              'version',
+              'private',
+              'packageManager',
+              'description',
+              'author',
+              'license',
+              'funding',
+              'homepage',
+              'repository',
+              'bugs',
+              'keywords',
+              'categories',
+              'sideEffects',
+              'exports',
+              'main',
+              'module',
+              'unpkg',
+              'jsdelivr',
+              'types',
+              'typesVersions',
+              'bin',
+              'icon',
+              'files',
+              'engines',
+              'activationEvents',
+              'contributes',
+              'scripts',
+              'dependencies',
+              'peerDependencies',
+              'peerDependenciesMeta',
+              'optionalDependencies',
+              'devDependencies',
+              'pnpm',
+              'overrides',
+              'resolutions',
+              'husky',
+              'simple-git-hooks',
+              'lint-staged',
+              'eslintConfig'
+            ]
+          },
+          {
+            pathPattern: '^(?:dev|peer|optional|bundled)?[Dd]ependencies$',
+            order: { type: 'asc' }
+          },
+          {
+            pathPattern: '^exports.*$',
+            order: ['types', 'require', 'import']
+          }
+        ]
+      }
+    }
+  ],
+  rules: {}
+};
