9c3ccdc46e0eda695e5c2819251b00f87c85f565
Soybean <honghuangdc@gmail.com>
2022-08-28 15:10:00 +0800

    fix(projects): fix react config, release v0.0.7

--- packages/react/index.js
+++ packages/react/index.js
@@ -1,5 +1,10 @@
 module.exports = {
   extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended', 'soybeanjs-ts'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   rules: {
     'react/react-in-jsx-scope': 'off'
   }
