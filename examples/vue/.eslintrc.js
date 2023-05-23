5bf88ea42937e25cc760bff3c4c0d73bb5fc53eb
Soybean <honghuangdc@gmail.com>
2023-05-23 21:42:49 +0800

    build(deps): update deps [升级依赖]

--- examples/vue/.eslintrc.js
+++ examples/vue/.eslintrc.js
@@ -2,5 +2,13 @@
  * @type {import('eslint').ESLint.ConfigData}
  */
 module.exports = {
-  extends: ['soybeanjs/vue']
+  extends: ['soybeanjs/vue'],
+  rules: {
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: ['demo']
+      }
+    ]
+  }
 };
---
+++ examples/vue/demo.tsx
@@ -0,0 +1,5 @@
+/** @jsxImportSource vue */
+
+import type { FunctionalComponent } from 'vue';
+
+export const SolidDemo: FunctionalComponent = () => <div>123</div>;
