2e65393544f27be744161619df34a82f1a479562
Soybean <soybeanjs@outlook.com>
2024-06-05 07:10:32 +0800

    chore(projects): update tsx require ts file

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,7 @@
-import 'tsx';
-import { createRequire } from 'node:module';
-import { defineConfig as defineConfig2 } from './dist/index.js';
+import { require } from 'tsx/cjs/api';
 
-const require = createRequire(import.meta.url);
-
-const { defineConfig: defineConfig1 } = require('./src/index.ts');
-
-const useBuild = false;
-
-/** @type {import('./src/index.ts').defineConfig} */
-const defineConfig = useBuild ? defineConfig2 : defineConfig1;
+/** @type {{ defineConfig: import('./src/index.ts').defineConfig }} */
+const { defineConfig } = require('./src/index.ts', import.meta.url);
 
 export default defineConfig({
   vue: true,
