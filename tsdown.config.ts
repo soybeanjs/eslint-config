7a4e15030dfc00a2220fdc42f6a2c1be7014b0f9
Soybean <soybeanjs@outlook.com>
2025-06-27 17:57:10 +0800

    refactor(projects): replace tsup with tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsdown';
+import pkg from './package.json';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  platform: 'node',
+  external: [...Object.keys(pkg.devDependencies)],
+  clean: true,
+  dts: true,
+  sourcemap: false,
+  minify: true,
+  format: ['esm'],
+  target: 'node18'
+});
