d6044b47336d6eb678c61944fcbcfe989c002c5f
Soybean <soybeanjs@outlook.com>
2025-07-05 13:22:55 +0800

    optimize(projects): optimize code

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,7 +4,7 @@ import pkg from './package.json';
 export default defineConfig({
   entry: ['src/index.ts'],
   platform: 'node',
-  external: [...Object.keys(pkg.devDependencies)],
+  external: Object.keys(pkg.devDependencies),
   clean: true,
   dts: true,
   sourcemap: false,
